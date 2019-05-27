adfc8f4ef4b99a33d0a5c0199f7005195a7d5860
clanntd_level5@yahoo.co.jp <clanntd_level5@yahoo.co.jp>
2019-05-28 05:16:57 +0900

    complete done

--- Computational-Fluid-Dynamics-Lec/src/linear_wave_eq/linear_wave_equation.f90
+++ Computational-Fluid-Dynamics-Lec/src/linear_wave_eq/linear_wave_equation.f90
@@ -10,9 +10,8 @@ program main
     real,dimension(100)::x
     real,dimension(100)::u
     real,dimension(100)::u_init
-    real,dimension(100)::u_
     integer num, n_step, i, j
-    real x_min, x_max, u1, u2, a, cfl, dx, dt ,l_x, x_i, c, lx
+    real x_min, x_max, u1, u2, cfl, dx, dt , x_i, c, lx
     ! 格子の最大点と最小点
     x_min = -5.0
     x_max = 5.0
@@ -26,6 +25,7 @@ program main
     lx = 2.0
     n_step = int(lx/dt/c) !移動距離から逆算
     print '(I5)', n_step
+    
     ! 格子
     x_i = x_min
     x(1) = x_i
@@ -38,12 +38,34 @@ program main
     u1 = 1.0
     u2 = 0.0
     call solve_ftcs(u1, u2, "../../output/ftcs1.csv")
+    call solve_LW(u1, u2, "../../output/LW1.csv")
+    call get_correct_solve(u1, u2, "../../output/correct1.csv")
 
     u1 = 0.0
     u2 = 1.0
     call solve_ftcs(u1, u2, "../../output/ftcs2.csv")
+    call solve_LW(u1, u2, "../../output/LW2.csv")
+    call get_correct_solve(u1, u2, "../../output/correct2.csv")
 contains
 
+    subroutine get_correct_solve(u1_init, u2_init, output)
+        ! 正しい解をゲットする。
+        real u1_init
+        real u2_init
+        character(*) output
+        open(5, file=output, status='replace')
+        write (5,*) (x(i), i=1,num)
+        do i=1, num
+            if(x(i)<=lx) then
+                u(i) = u1_init
+            else
+                u(i) = u2_init
+            end if
+        end do
+        write (5, *) (u(i), i=1,num)
+        close(5)
+
+    end subroutine
     subroutine solve_ftcs(u1_init, u2_init, output)
         ! FTCSを解く
         ! Arg:
@@ -84,4 +106,43 @@ contains
         close(20)
     end subroutine
 
+    subroutine solve_LW(u1_init, u2_init, output)
+        !Lax-Wendroffスキームで解いてみる。
+        ! Arg:
+        !   u1_init(float) 初期条件(x<=0)
+        !   u2_init(float) 初期条件(x>0)
+        ! Return:
+        !   csv => output
+        !       row[0]は位置
+        !       row[1:]は速度の遷移
+        real u1_init
+        real u2_init
+        character(*) output
+
+        ! 初期条件
+        do i=1, num
+            if(x(i)<=0) then
+                u_init(i) = u1_init
+            else
+                u_init(i) = u2_init
+            end if
+        end do
+
+        !初期条件をコピー
+        do i=1, num
+            u(i) = u_init(i)
+        end do
+
+        !出力ファイル
+        open(18, file=output, status='replace')
+        write (18,*) (x(i), i=1,num)
+        write(18,*) (u_init(i), i=1,num)
+        do j=1, n_step !時間ステップ
+            do i=2,num-1 !位置
+                u(i) = u(i)-cfl/2*(u(i+1)-u(i-1))+cfl**2/2*(u(i+1)-2*u(i)+u(i-1))
+            end do
+            write (18,*) (u(i),i=1,num)
+        end do
+        close(18)
+    end subroutine
 end program main
--- Computational-Fluid-Dynamics-Lec/src/plotting.py
+++ Computational-Fluid-Dynamics-Lec/src/plotting.py
@@ -5,7 +5,7 @@ import pandas as pd
 import pathlib
 import glob
 
-def visualize(filename, all_plot=False, save_fig=True):
+def visualize(filename, correct_filename, all_plot=False, save_fig=True):
     """結果をプロットするだけ
     Arg:
         filename(str) <= .csv
@@ -14,16 +14,24 @@ def visualize(filename, all_plot=False, save_fig=True):
         None <= (save_fig==False)
     """
     rows = []
+    corrects = []
     with open(filename, mode="r") as f:
         reader = csv.reader(f, delimiter=' ', skipinitialspace=True)
         for row in reader:
             row = row[:-1]
             row = list(map(float, row))
             rows.append(row)
-    
+
+    with open(correct_filename, mode="r") as f:
+        reader = csv.reader(f, delimiter=' ', skipinitialspace=True)
+        for row in reader:
+            row = row[:-1]
+            row = list(map(float, row))
+            corrects.append(row)
     plt.clf()
     X = rows[0]
     U = rows[1:]
+    u_right = corrects[1]
     if all_plot:
         for u in U:
             plt.plot(X,u)
@@ -32,17 +40,24 @@ def visualize(filename, all_plot=False, save_fig=True):
         u_end = U[-1]
         plt.plot(X,u_start, label="initial")
         plt.plot(X, u_end, label="end")
+        plt.plot(X, u_right, label="correct")
 
     file_path = pathlib.Path(filename)
     file_stem = file_path.stem
     plt.legend()
     if save_fig:
-        plt.savefig(file_stem+".png")
+        plt.savefig("output/"+file_stem+".png")
     plt.show()
     return None
 
 now_path = pathlib.Path.cwd()
+correct1_path = now_path/"output"/"correct1.csv"
+correct2_path = now_path/"output"/"correct2.csv"
 ftcs1_path = now_path/"output"/"ftcs1.csv"
-visualize(ftcs1_path)
 ftcs2_path = now_path/"output"/"ftcs2.csv"
-visualize(ftcs2_path)
+LW1_path = now_path/"output"/"LW1.csv"
+LW2_path = now_path/"output"/"LW2.csv"
+visualize(ftcs1_path, correct1_path)
+visualize(ftcs2_path, correct2_path)
+visualize(LW1_path, correct1_path)
+visualize(LW2_path, correct2_path)
